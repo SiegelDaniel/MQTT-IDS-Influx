7dab83dd9e727ea73a751c71a93e74fe05a02107
scd-ds <daniel.siegel0@googlemail.com>
2019-09-17 14:46:10 +0200

    testing the attack

--- test/util/attack.py
+++ test/util/attack.py
@@ -6,9 +6,6 @@ import paho.mqtt.client as mqtt
 class Attack(object):
 
     def __init__(self):
-        #start polling thread to simulate normal behaviour
-        x = threading.Thread(target=self.poll_heise)
-        x.start()
 
         self.client = mqtt.Client()
         self.client.on_connect = self.on_connect
@@ -25,6 +22,8 @@ class Attack(object):
     def on_connect(self):
         print("Connected to MQTT broker at test.mosquitto.org")
         self.client.subscribe("ATTACK")
+        thread = threading.Thread(target=self.poll_heise)
+        thread.start()
     
     def on_message(self):
         for i in range(1,80):
@@ -37,7 +36,8 @@ class Attack(object):
         new_file.close()
 
 
-    
+if __name__ == "__main__":
+    attack = Attack()
 
     
 
